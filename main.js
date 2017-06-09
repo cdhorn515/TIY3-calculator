b6fd92c0bca2c1abd95ac9feaa8a78588dea597b
Christina Cornell <cdhorn515@gmail.com>
2017-06-09 18:36:47 -0400

    created html, style, and js files,tested js and style. added notes for js and built basic layout for calculator

---
+++ main.js
@@ -0,0 +1,43 @@
+// "use strict";
+  //
+  // function that executes when user clicks on clear button
+  // function clearNumbers() {
+  //
+  // }
+
+  //
+  // everytime a number is clicked it is stored into the numberStorageNode and printed to the screen
+  //use addEventListener "click"
+  // function storeNumbers() {
+      numberStorageNode.push(num);
+      numberViewerNode += num;
+
+  //
+   }
+  // // this function gets called every time an operator is clicked
+  // //accepts number and operator, operator is equal to a number in an object
+  // function doTheMath(number, operator){
+
+      if (operatorObj.property[x])
+  //   for (var i = 0; i<numberStorageNode.length; i++);
+  //    doMath += numberStorageNode + operatorObj[property];
+  //    return
+  // }
+var numberViewer = "";
+var numberStorageNode = [];
+var doMath = '9';
+var sumTotal = 0;
+
+var operatorObj = {
+ plus: '+',
+ minus:'-',
+ multiply: '*',
+ divide: '/',
+ equal: '=',
+ decimal: '.'
+ //clear = clearNumbers();
+}
+
+sumTotal = "5" + operatorObj.minus + "1"
+
+console.log(sumTotal);
