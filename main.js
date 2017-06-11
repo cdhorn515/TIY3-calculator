9d83ec3b1aae0add83f387af3a6257990e8e16cd
Christina Cornell <cdhorn515@gmail.com>
2017-06-11 13:12:51 -0400

    parseFloat numbers and add to sumTotal. still need to fix getting calcs done, sumTotal changed from number type to string

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // "use strict";
 
 var runningTotal = "";
+var sumTotal = 0;
 
-function total(){
+function total() {
   return runningTotal = runningTotal + displayNode.textContent;
 }
 
@@ -15,120 +16,156 @@ var displayNode = document.getElementById('display');
 displayNode.setAttribute('style', 'fontFamily: Helvetica, sans-serif; fontWeight: lighter; fontSize: 55px; textAlign: right;');
 
 var clearNode = document.getElementById('clear');
-clearNode.addEventListener("click", function(){
+clearNode.addEventListener("click", function() {
   runningTotal = "";
   displayNode.textContent = "0";
   console.log('runningTotal is now ' + runningTotal);
-  alert("it works! You clicked the " + clearNode.attributes[1].nodeValue + " button")});
+  alert("it works! You clicked the " + clearNode.attributes[1].nodeValue + " button")
+});
 
 var equalNode = document.getElementById('equal');
-equalNode.addEventListener("click", function(){
+equalNode.addEventListener("click", function() {
   var totalArray = runningTotal.split(' ');
-  console.log(totalArray);
-  alert("it works! You clicked the " + equalNode.attributes[1].nodeValue +  " sign")});
+  for (i = 0; i < totalArray.length; i++) {
+    if (i % 2 === 0) {
+      parseFloat(totalArray[i]);
+      console.log(totalArray);
+      sumTotal = sumTotal + totalArray[i];
+    } else sumTotal = sumTotal + totalArray[i];
+
+    // } else if (totalArray[i] == /) {
+    //   totalArray[i] = /;
+    // } else if (totalArray[i] == *) {
+    //   totalArray[i] = *;
+    // } else if (totalArray[i] == -) {
+    //   totalArray[i] = -;
+    // } else if (totalArray[i] == +) {
+    //   totalArray[i] = +;
+    // }
+    // sumTotal = sumTotal + totalArray[i];
+  }
+  console.log('here' + totalArray);
+  console.log('here' + sumTotal);
+  alert("it works! You clicked the " + equalNode.attributes[1].nodeValue + " sign")
+});
 
 var decimalNode = document.getElementById('.');
 //include in function to turn off eventListener
 decimalNode.addEventListener("click",
- function(){
-   //returns undefined.
-  //  var runningTotal = runningTotal + decimalNode.attributes[1].nodeValue;
-   runningTotal = runningTotal + '.';
-   console.log(runningTotal);
-   alert("it works! You clicked the " + decimalNode.attributes[1].nodeValue + " symbol")});
+  function() {
+    //returns undefined.
+    //  var runningTotal = runningTotal + decimalNode.attributes[1].nodeValue;
+    runningTotal = runningTotal + '.';
+    console.log(runningTotal);
+    alert("it works! You clicked the " + decimalNode.attributes[1].nodeValue + " symbol")
+  });
 
 var allNumbers = document.querySelectorAll('.num');
 
 //div with class of operator, returns an array (includes childNode, but need operatorTextNode to access symbol)
 var allOperators = document.querySelectorAll('.operator');
 // for (var i = 0; i < allOperators.length; i++) {
-  // var operatorStore = allOperators[i];
+// var operatorStore = allOperators[i];
 var divideNode = document.getElementById('divide');
-divideNode.addEventListener("click", function(){
+divideNode.addEventListener("click", function() {
   runningTotal = runningTotal + " " + operatorTextNode[0].childNodes[0].data + " ";
   console.log(runningTotal);
-  alert("it works! you clicked on the " + operatorTextNode[0].childNodes[0].data + " operator")});
-  // allOperators.appendChild(operatorStore);
+  alert("it works! you clicked on the " + operatorTextNode[0].childNodes[0].data + " operator")
+});
+// allOperators.appendChild(operatorStore);
 var multiplyNode = document.getElementById('multiply');
- multiplyNode.addEventListener("click", function(){
-   runningTotal = runningTotal + " " + operatorTextNode[1].childNodes[0].data + " ";
-   console.log(runningTotal);
-   alert("it works! you clicked on the " + operatorTextNode[1].childNodes[0].data + " operator")});
-  // allOperators.appendChild(operatorStore);
+multiplyNode.addEventListener("click", function() {
+  runningTotal = runningTotal + " " + operatorTextNode[1].childNodes[0].data + " ";
+  console.log(runningTotal);
+  alert("it works! you clicked on the " + operatorTextNode[1].childNodes[0].data + " operator")
+});
+// allOperators.appendChild(operatorStore);
 var minusNode = document.getElementById('minus');
-  minusNode.addEventListener("click", function(){
-    runningTotal = runningTotal + " " + operatorTextNode[2].childNodes[0].data + " ";
-    console.log(runningTotal);
-    alert("it works! you clicked on the " + operatorTextNode[2].childNodes[0].data + " operator")});
-  // allOperators.appendChild(operatorStore);
+minusNode.addEventListener("click", function() {
+  runningTotal = runningTotal + " " + operatorTextNode[2].childNodes[0].data + " ";
+  console.log(runningTotal);
+  alert("it works! you clicked on the " + operatorTextNode[2].childNodes[0].data + " operator")
+});
+// allOperators.appendChild(operatorStore);
 var plusNode = document.getElementById('plus');
-  plusNode.addEventListener("click", function(){
-    runningTotal = runningTotal + " " + operatorTextNode[3].childNodes[0].data + " ";
-    console.log(runningTotal);
-    alert("it works! you clicked on the " + operatorTextNode[3].childNodes[0].data + " operator")});
-  // allOperators.appendChild(operatorStore);
+plusNode.addEventListener("click", function() {
+  runningTotal = runningTotal + " " + operatorTextNode[3].childNodes[0].data + " ";
+  console.log(runningTotal);
+  alert("it works! you clicked on the " + operatorTextNode[3].childNodes[0].data + " operator")
+});
+// allOperators.appendChild(operatorStore);
 
-  var zeroNode = document.getElementById('0');
-  zeroNode.addEventListener("click", function(){
-    displayNode.textContent = "0";
-    runningTotal = runningTotal + displayNode.textContent;
-    console.log(runningTotal);
-    alert("it works! you clicked number " + zeroNode.attributes[1].nodeValue)});
-  var oneNode = document.getElementById('1');
-  oneNode.addEventListener("click", function(){
-    displayNode.textContent = "1";
-    runningTotal = runningTotal + displayNode.textContent;
-    console.log(runningTotal);
-    alert("it works! you clicked number " + oneNode.attributes[1].nodeValue)});
-  var twoNode = document.getElementById('2');
-  twoNode.addEventListener("click", function(){
-    displayNode.textContent = "2";
-    runningTotal = runningTotal + displayNode.textContent;
-    console.log(runningTotal);
-    alert("it works! you clicked number " + twoNode.attributes[1].nodeValue)});
-  var threeNode = document.getElementById('3');
-  threeNode.addEventListener("click", function(){
-    displayNode.textContent = "3";
-    runningTotal = runningTotal + displayNode.textContent;
-    console.log(runningTotal);
-    alert("it works! you clicked number " + threeNode.attributes[1].nodeValue)});
-  var fourNode = document.getElementById('4');
-  fourNode.addEventListener("click", function(){
-    displayNode.textContent = "4";
-    runningTotal = runningTotal + displayNode.textContent;
-    console.log(runningTotal);
-    alert("it works! you clicked number " + fourNode.attributes[1].nodeValue)});
-  var fiveNode = document.getElementById('5');
-  // fiveNode.addEventListener("click", displayNumber);
-  fiveNode.addEventListener("click", function(){
-    displayNode.textContent = "5";
-    runningTotal = runningTotal + displayNode.textContent;
-    console.log(runningTotal);
-    alert("it works! you clicked number " + fiveNode.attributes[1].nodeValue)});
-  var sixNode = document.getElementById('6');
-  sixNode.addEventListener("click", function(){
-    displayNode.textContent = "6";
-    runningTotal = runningTotal + displayNode.textContent;
-    console.log(runningTotal);
-    alert("it works! you clicked number " + sixNode.attributes[1].nodeValue)});
-  var sevenNode = document.getElementById('7');
-  sevenNode.addEventListener("click", function(){
-    displayNode.textContent = "7";
-    runningTotal = runningTotal + displayNode.textContent;
-    console.log(runningTotal);
-    alert("it works! you clicked number " + sevenNode.attributes[1].nodeValue)});
-  var eightNode = document.getElementById('8');
-  eightNode.addEventListener("click", function(){
-    displayNode.textContent = "8";
-    runningTotal = runningTotal + displayNode.textContent;
-    console.log(runningTotal);
-    alert("it works! you clicked number " + eightNode.attributes[1].nodeValue)});
-  var nineNode = document.getElementById('9');
-  nineNode.addEventListener("click", function(){
-    displayNode.textContent = "9";
-    runningTotal = runningTotal + displayNode.textContent;
-    console.log(runningTotal);
-    alert("it works! you clicked number " + nineNode.attributes[1].nodeValue)});
+var zeroNode = document.getElementById('0');
+zeroNode.addEventListener("click", function() {
+  displayNode.textContent = "0";
+  runningTotal = runningTotal + displayNode.textContent;
+  console.log(runningTotal);
+  alert("it works! you clicked number " + zeroNode.attributes[1].nodeValue)
+});
+var oneNode = document.getElementById('1');
+oneNode.addEventListener("click", function() {
+  displayNode.textContent = "1";
+  runningTotal = runningTotal + displayNode.textContent;
+  console.log(runningTotal);
+  alert("it works! you clicked number " + oneNode.attributes[1].nodeValue)
+});
+var twoNode = document.getElementById('2');
+twoNode.addEventListener("click", function() {
+  displayNode.textContent = "2";
+  runningTotal = runningTotal + displayNode.textContent;
+  console.log(runningTotal);
+  alert("it works! you clicked number " + twoNode.attributes[1].nodeValue)
+});
+var threeNode = document.getElementById('3');
+threeNode.addEventListener("click", function() {
+  displayNode.textContent = "3";
+  runningTotal = runningTotal + displayNode.textContent;
+  console.log(runningTotal);
+  alert("it works! you clicked number " + threeNode.attributes[1].nodeValue)
+});
+var fourNode = document.getElementById('4');
+fourNode.addEventListener("click", function() {
+  displayNode.textContent = "4";
+  runningTotal = runningTotal + displayNode.textContent;
+  console.log(runningTotal);
+  alert("it works! you clicked number " + fourNode.attributes[1].nodeValue)
+});
+var fiveNode = document.getElementById('5');
+// fiveNode.addEventListener("click", displayNumber);
+fiveNode.addEventListener("click", function() {
+  displayNode.textContent = "5";
+  runningTotal = runningTotal + displayNode.textContent;
+  console.log(runningTotal);
+  alert("it works! you clicked number " + fiveNode.attributes[1].nodeValue)
+});
+var sixNode = document.getElementById('6');
+sixNode.addEventListener("click", function() {
+  displayNode.textContent = "6";
+  runningTotal = runningTotal + displayNode.textContent;
+  console.log(runningTotal);
+  alert("it works! you clicked number " + sixNode.attributes[1].nodeValue)
+});
+var sevenNode = document.getElementById('7');
+sevenNode.addEventListener("click", function() {
+  displayNode.textContent = "7";
+  runningTotal = runningTotal + displayNode.textContent;
+  console.log(runningTotal);
+  alert("it works! you clicked number " + sevenNode.attributes[1].nodeValue)
+});
+var eightNode = document.getElementById('8');
+eightNode.addEventListener("click", function() {
+  displayNode.textContent = "8";
+  runningTotal = runningTotal + displayNode.textContent;
+  console.log(runningTotal);
+  alert("it works! you clicked number " + eightNode.attributes[1].nodeValue)
+});
+var nineNode = document.getElementById('9');
+nineNode.addEventListener("click", function() {
+  displayNode.textContent = "9";
+  runningTotal = runningTotal + displayNode.textContent;
+  console.log(runningTotal);
+  alert("it works! you clicked number " + nineNode.attributes[1].nodeValue)
+});
 
 
 //child of calc-btn, 4 operands (+-*/)
@@ -152,11 +189,11 @@ function changeTextColor() {
 
 // // this function gets called every time an operator is clicked
 // //accepts number and operator, operator is equal to a number in an object
-function doTheMath(number, operator){
-    if (operatorObj.property[x])
-  for (var i = 0; i<numberStorageNode.length; i++);
-   doMath += numberStorageNode + operatorObj[property];
-   return
+function doTheMath(number, operator) {
+  if (operatorObj.property[x])
+    for (var i = 0; i < numberStorageNode.length; i++);
+  doMath += numberStorageNode + operatorObj[property];
+  return
 }
 
 
@@ -168,35 +205,35 @@ function doTheMath(number, operator){
 
 //allNumbers returns an array, loop through the array to return the text child allNumbers[index of allNumbers].attributes[1].nodeValue
 var numberStorageNode = {};
-  for (var i = 0; i < allNumbers.length; i++) {
-    numberStorageNode = document.getElementById('3');
+for (var i = 0; i < allNumbers.length; i++) {
+  numberStorageNode = document.getElementById('3');
 }
 
-function displayNumber(){
+function displayNumber() {
   //number shows up in upper lh corner, very small
 
   // displayNode.setAttribute('style', 'fontSize: 55px; textAlign: right; paddingRight: 24px;');
   displayNode.textContent = '5';
 }
-  // for (var i = 0; i < numberStorageNode.length; i++) {
-  //  if (num == numberStorageNode[i].attributes.value) {
-  //     console.log('here', num, numberStorageNode[i]);
-  //   }
-  // }
-    // numberStorageNode[i].setAttribute('id', allNumbers.id);
-    // console.log(numberStorageNode);NOTE might have to go back to numberStorageNode
-    // numberStorageNode.addEventListener("click", function showNumber(){
-      // for (var i = 0; i < allNumbers.length; i++){
-      //   if (numberStorageNode === i)
-        // alert("you clicked on a number ");
+// for (var i = 0; i < numberStorageNode.length; i++) {
+//  if (num == numberStorageNode[i].attributes.value) {
+//     console.log('here', num, numberStorageNode[i]);
+//   }
 // }
-      // alert("it works! You clicked on a number" + i);
-    // });
+// numberStorageNode[i].setAttribute('id', allNumbers.id);
+// console.log(numberStorageNode);NOTE might have to go back to numberStorageNode
+// numberStorageNode.addEventListener("click", function showNumber(){
+// for (var i = 0; i < allNumbers.length; i++){
+//   if (numberStorageNode === i)
+// alert("you clicked on a number ");
+// }
+// alert("it works! You clicked on a number" + i);
+// });
 
-    // returns a list of divs with each number
-    // console.log(numberStorageNode);
+// returns a list of divs with each number
+// console.log(numberStorageNode);
 
-    // console.log(numberStorageNode);
+// console.log(numberStorageNode);
 
 // operatorStore.addEventListener("click", function(){alert("it works! you clicked on an operator")});
 
@@ -246,9 +283,9 @@ function displayNumber(){
 // for (var i = 0; i < allOperators.length; i++) {
 //   var operatorNode = allOperators[i];
 //   operatorNode.setAttribute('value', operatorNode.attributes[1].value);
-  // console.log(operatorNode.attributes[1].value);
-  // console.log('operatorNode' + [i] + ' value= ', operatorNode.attributes[1].value);
-  // console.log(operatorNode);
+// console.log(operatorNode.attributes[1].value);
+// console.log('operatorNode' + [i] + ' value= ', operatorNode.attributes[1].value);
+// console.log(operatorNode);
 
 
 
