a9e182e0b915736a236ec9ec3a4c0811334013b7
Christina Cornell <cdhorn515@gmail.com>
2017-06-12 23:54:15 -0400

    still testing limiting decimal places

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function addToDisplay(){
    displayNode.textContent = displayNode.textContent + text;
    if (text === '/' || text === '*' || text === '-' || text === '+'){
      runningTotal = runningTotal + " " + text + " ";
-
+    //  return decimalClicks = 0;
    } else runningTotal = runningTotal + text;
    return runningTotal;
 }
@@ -29,22 +29,24 @@ for (var i = 0; i < allOperands.length; i++) {
     if (event.target.textContent === "C"){
       runningTotal = "";
       displayNode.textContent = "";
-      hasDecimalBeenClicked = 0;
+      // hasDecimalBeenClicked = 0;
       return;
     } else if (event.target.textContent === 'X'){
       runningTotal = runningTotal + " * ";
       displayNode.textContent = displayNode.textContent + "*";
-      hasDecimalBeenClicked = 0;
+      // hasDecimalBeenClicked = 0;
       return;
     } else if (event.target.textContent === '.') {
-      if (hasDecimalBeenClicked < 1) {
-        addToDisplay();
-        hasDecimalBeenClicked++;
-      } return;
+      if (decimalClicks < 1) {
+        // addToDisplay();
+        // hasDecimalBeenClicked++;
+        // return;
+      }
     } else if (event.target.textContent === '=') {
-    return sumOfNumbers();
+      return sumOfNumbers();
     }
-     addToDisplay();
+    hasDecimalBeenClicked();
+    addToDisplay();
   });
 }
 
@@ -66,14 +68,20 @@ function sumOfNumbers() {
        if (i % 2 != 0 && totalArray[i] === '+') {
        sumTotal = sumTotal + parseFloat(totalArray[i+1]);
      }
-   } hasDecimalBeenClicked = 0;
+   } hasDecimalBeenClicked();
      displayNode.textContent = sumTotal;
   }
+
+function hasDecimalBeenClicked(){
+  if (event.target.textContent === '.' && decimalClicks === 0) {
+    return decimalClicks = 1;
+  } else return decimalClicks = 0;
+}
 
   var totalArray;
   var runningTotal = "";
   var sumTotal;
-  var hasDecimalBeenClicked;
+  var decimalClicks = 0;
   var calculatorNode = document.getElementById('calculator');
 
 
@@ -85,4 +93,5 @@ if (runningTotal.length > 7) {
 displayNode.setAttribute('style', 'fontFamily: Helvetica, sans-serif; fontWeight: lighter; fontSize: 55px; textAlign: right;');
 }
 
+//end of use strict IIFE
 })();
