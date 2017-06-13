9b2a430a77d1deb9cddcf24781a2d39be6f4b271
Christina Cornell <cdhorn515@gmail.com>
2017-06-13 16:56:49 -0400

    calculator not workng

--- main.js
+++ main.js
@@ -1,102 +1,113 @@
-(function(){
- "use strict";
+(function() {
+    "use strict";
 
-function addToDisplay(){
-   var text = event.target.textContent;
-   displayNode.textContent = displayNode.textContent + text;
-   if (text === '/' || text === '*' || text === '-' || text === '+'){
-     runningTotal = runningTotal + " " + text + " ";
-    //  return decimalClicks = 0;
-   } else runningTotal = runningTotal + text;
-    //  return runningTotal;
-    displayNode.textContent = runningTotal;
-}
-
-function total() {
-  return runningTotal = runningTotal + displayNode.textContent;
-}
-
-//gets all numbers by class, sets up click event, adds to runningTotal and to display
-var allNumbers = document.querySelectorAll('.number');
-  for (var i = 0; i < allNumbers.length; i++) {
-  allNumbers[i].addEventListener('click', function(){
-    return addToDisplay();
-  });
-  }
-
-var allOperands = document.querySelectorAll('.operator');
-for (var i = 0; i < allOperands.length; i++) {
-  allOperands[i].addEventListener('click', function(){
-    if (event.target.textContent === "C"){
-      runningTotal = "";
-      displayNode.textContent = "";
-      // hasDecimalBeenClicked = 0;
-      return;
-    } else if (event.target.textContent === 'X'){
-      runningTotal = runningTotal + " * ";
-      displayNode.textContent = displayNode.textContent + "*";
-      // hasDecimalBeenClicked = 0;
-      return;
-    } else if (event.target.textContent === '.') {
-      if (decimalClicks === 1) {
-        // addToDisplay();
-        // hasDecimalBeenClicked++;
-        return;
+    function addToDisplay() {
+      var text = event.target.textContent;
+      displayNode.textContent = displayNode.textContent + text;
+      if (text === '/' || text === '*' || text === '-' || text === '+') {
+        runningTotal = runningTotal + " " + text + " ";
+        //  return decimalClicks = 0;
+      } else {
+        runningTotal = runningTotal + text;
       }
-    } else if (event.target.textContent === '=') {
-
-      return sumOfNumbers();
+      displayNode.textContent = runningTotal;
+      return runningTotal;
     }
-    hasDecimalBeenClicked();
-    addToDisplay();
-  });
-}
 
-function sumOfNumbers() {
-  var totalArray = runningTotal.split(' ');
-  for (var i = 0; i < totalArray.length; i++) {
-    if (i === 0) {
-      sumTotal = parseFloat(totalArray[i]);
-    } else
-      if (i % 2 != 0 && totalArray[i] === '/') {
-       sumTotal = sumTotal / parseFloat(totalArray[i+1]);
-    } else
-      if (i % 2 != 0 && totalArray[i] === '*') {
-       sumTotal = sumTotal * parseFloat(totalArray[i+1]);
-    } else
-      if (i % 2 != 0 && totalArray[i] === '-') {
-       sumTotal = sumTotal - parseFloat(totalArray[i+1]);
-     } else
-       if (i % 2 != 0 && totalArray[i] === '+') {
-       sumTotal = sumTotal + parseFloat(totalArray[i+1]);
-     }
-   } runningTotal = "";
-     hasDecimalBeenClicked();
-     displayNode.textContent = sumTotal;
-  }
+    function total(runningTotal) {
+      return runningTotal = runningTotal + displayNode.textContent;
+    }
 
-function hasDecimalBeenClicked(){
-  decimalClicks = 0;
-  if (event.target.textContent === '.' && decimalClicks === 1) {
-    return;
-  } else return decimalClicks++;
+    //gets all numbers by class, sets up click event, adds to runningTotal and to display
+    var allNumbers = document.querySelectorAll('.number');
+    for (var i = 0; i < allNumbers.length; i++) {
+      allNumbers[i].addEventListener('click', function() {
+        return addToDisplay();
+      });
+    }
 
-}
+    var allOperands = document.querySelectorAll('.operator');
+    for (var i = 0; i < allOperands.length; i++) {
+      allOperands[i].addEventListener('click', function() {
+        if (event.target.textContent === "C") {
+          runningTotal = "";
+          displayNode.textContent = "";
+        decimalClicks = 0;
+          return;
+        } else if (event.target.textContent === 'X') {
+          runningTotal = runningTotal + " * ";
+          displayNode.textContent = displayNode.textContent + "*";
+          // hasDecimalBeenClicked = 0;
+          return;
+        } else if (event.target.textContent === '.') {
+          // hasDecimalBeenClicked();
+          if (decimalClicks === 0) {
+            addToDisplay();
+            decimalClicks++;
+            return;
+          }
+        } else if (event.target.textContent === '=') {
+          sumOfNumbers();
+          // runningTotal = 0;
+        }
+        // hasDecimalBeenClicked();
+      });
+    }
 
-  var totalArray;
-  var runningTotal = "";
-  var sumTotal;
-  var decimalClicks = 0;
-  var calculatorNode = document.getElementById('calculator');
+    function sumOfNumbers() {
+      var totalArray = runningTotal.split(' ');
+      for (var i = 0; i < totalArray.length; i++) {
+        if (i === 0) {
+          sumTotal = parseFloat(totalArray[i]);
+        } else
+        if (i % 2 != 0 && totalArray[i] === '/') {
+          sumTotal = sumTotal / parseFloat(totalArray[i + 1]);
+        } else
+        if (i % 2 != 0 && totalArray[i] === '*') {
+          sumTotal = sumTotal * parseFloat(totalArray[i + 1]);
+        } else
+        if (i % 2 != 0 && totalArray[i] === '-') {
+          sumTotal = sumTotal - parseFloat(totalArray[i + 1]);
+        } else
+        if (i % 2 != 0 && totalArray[i] === '+') {
+          sumTotal = sumTotal + parseFloat(totalArray[i + 1]);
+        }
+      }
+      // runningTotal = "";
+        displayNode.textContent = sumTotal;
+        // hasDecimalBeenClicked();
+
+      }
+
+      function hasDecimalBeenClicked(){
+        if (event.target.textContent === '.' && decimalClicks === 1) {
+          return;
+        } else return decimalClicks++;
+      }
+
+      var totalArray;
+      var runningTotal = "";
+      var sumTotal;
+      var decimalClicks = 0;
+      var calculatorNode = document.getElementById('calculator');
 
 
-var displayNode = document.getElementById('display');
-// FIXME set style for display so numbers added will show correct size and place
-if (runningTotal.length > 7) {
-  displayNode.setAttribute('style', 'fontSize: 40px;');
-} else {
-displayNode.setAttribute('style', 'fontFamily: Helvetica, sans-serif; fontWeight: lighter; fontSize: 55px; textAlign: right;');
-}
+      var displayNode = document.getElementById('display');
 
-//end of use strict IIFE
-})();
+      
+      // FIXME set style for display so numbers added will show correct size and place add to event listener
+      if (runningTotal.length > 7) {
+        displayNode.setAttribute('style', 'fontSize: 40px;');
+      } else {
+        displayNode.setAttribute('style', 'fontFamily: Helvetica, sans-serif; fontWeight: lighter; fontSize: 55px; textAlign: right;');
+      }
+
+      //
+      // function hasOperandBeenClicked(){
+      //   if (event.target.textContent === '.' || event.target.textContent === '/' || event.target.textContent === '*' || event.target.textContent === '+' || event.target.textContent === '-' )
+      //   if (if clicks.target === 0) {
+      //     return;
+      //   }
+      // }
+      //end of use strict IIFE
+    })();
--- testing.js
+++ testing.js
@@ -25,6 +25,8 @@
 //
 //   })
 //  }
+
+//this works, displays all numbers to displayNode and keeps track in runningTotal
 var displayNode = document.getElementById('display');
 var runningTotal = "";
 var allNumbers = document.querySelectorAll('.number');
@@ -37,7 +39,150 @@ var allNumbers = document.querySelectorAll('.number');
   });
 console.log(runningTotal);
   }
+
+
  // (){alert("it works! you clicked number " + zeroNode.attributes[1].nodeValue)});
+ var zeroNode = document.getElementById('0');
+ zeroNode.addEventListener("click", function() {
+   displayNode.textContent = displayNode.textContent + "0";
+   // runningTotal = runningTotal + displayNode.textContent;
+   runningTotal = runningTotal + zeroNode.attributes[1].nodeValue;
+   // console.log(runningTotal);
+   // alert("it works! you clicked number " + zeroNode.attributes[1].nodeValue)
+ });
+ var oneNode = document.getElementById('1');
+ oneNode.addEventListener("click", function() {
+   displayNode.textContent = displayNode.textContent + "1";
+   runningTotal = runningTotal + oneNode.attributes[1].nodeValue;
+   // runningTotal = runningTotal + displayNode.textContent;
+   // console.log(runningTotal);
+   // alert("it works! you clicked number " + oneNode.attributes[1].nodeValue)
+ });
+ var twoNode = document.getElementById('2');
+ twoNode.addEventListener("click", function() {
+   displayNode.textContent = displayNode.textContent + "2";
+   runningTotal = runningTotal + twoNode.attributes[1].nodeValue;
+   // console.log(runningTotal);
+   // alert("it works! you clicked number " + twoNode.attributes[1].nodeValue)
+ });
+ var threeNode = document.getElementById('3');
+ threeNode.addEventListener("click", function() {
+   displayNode.textContent = displayNode.textContent + "3";
+   runningTotal = runningTotal + threeNode.attributes[1].nodeValue;
+   // console.log(runningTotal);
+   // alert("it works! you clicked number " + threeNode.attributes[1].nodeValue)
+ });
+ var fourNode = document.getElementById('4');
+ fourNode.addEventListener("click", function() {
+   displayNode.textContent = displayNode.textContent + "4";
+   runningTotal = runningTotal + fourNode.attributes[1].nodeValue;
+   // console.log(runningTotal);
+   // alert("it works! you clicked number " + fourNode.attributes[1].nodeValue)
+ });
+ var fiveNode = document.getElementById('5');
+ // fiveNode.addEventListener("click", displayNumber);
+ fiveNode.addEventListener("click", function() {
+   displayNode.textContent = displayNode.textContent + "5";
+   runningTotal = runningTotal + fiveNode.attributes[1].nodeValue;
+   // console.log(runningTotal);
+   // alert("it works! you clicked number " + fiveNode.attributes[1].nodeValue)
+ });
+ var sixNode = document.getElementById('6');
+ sixNode.addEventListener("click", function() {
+   displayNode.textContent = displayNode.textContent + "6";
+   runningTotal = runningTotal + sixNode.attributes[1].nodeValue;
+   // console.log(runningTotal);
+   // alert("it works! you clicked number " + sixNode.attributes[1].nodeValue)
+ });
+ var sevenNode = document.getElementById('7');
+ sevenNode.addEventListener("click", function() {
+   displayNode.textContent = displayNode.textContent + "7";
+   runningTotal = runningTotal + sevenNode.attributes[1].nodeValue;
+   // console.log(runningTotal);
+   // alert("it works! you clicked number " + sevenNode.attributes[1].nodeValue)
+ });
+ var eightNode = document.getElementById('8');
+ eightNode.addEventListener("click", function() {
+   displayNode.textContent = displayNode.textContent + "8";
+   runningTotal = runningTotal + eightNode.attributes[1].nodeValue;
+   // console.log(runningTotal);
+   // alert("it works! you clicked number " + eightNode.attributes[1].nodeValue)
+ });
+ var nineNode = document.getElementById('9');
+ nineNode.addEventListener("click", function() {
+   displayNode.textContent = displayNode.textContent + "9";
+   runningTotal = runningTotal + nineNode.attributes[1].nodeValue;
+   // console.log(runningTotal);
+   // alert("it works! you clicked number " + nineNode.attributes[1].nodeValue)
+ });
+
+ var allOperators = document.querySelectorAll('.operator');
+ // for (var i = 0; i < allOperators.length; i++) {
+ // var operatorStore = allOperators[i];
+ var divideNode = document.getElementById('divide');
+   divideNode.addEventListener("click", function() {
+   displayNode.textContent = runningTotal + '/';
+   // displayNode.textContent = displayNode.textContent + '/';
+   runningTotal = runningTotal + " " + operatorTextNode[0].childNodes[0].data + " ";
+   hasDecimalBeenClicked = 0;
+   // console.log(runningTotal);
+   // alert("it works! you clicked on the " + operatorTextNode[0].childNodes[0].data + " operator")
+ });
+ var multiplyNode = document.getElementById('multiply');
+   multiplyNode.addEventListener("click", function() {
+   displayNode.textContent = runningTotal + '*';
+   runningTotal = runningTotal + " " + operatorTextNode[1].childNodes[0].data + " ";
+   hasDecimalBeenClicked = 0;
+   // console.log(runningTotal);
+   // alert("it works! you clicked on the " + operatorTextNode[1].childNodes[0].data + " operator")
+ });
+ var minusNode = document.getElementById('minus');
+   minusNode.addEventListener("click", function() {
+   displayNode.textContent = runningTotal + '-';
+   runningTotal = runningTotal + " " + operatorTextNode[2].childNodes[0].data + " ";
+   hasDecimalBeenClicked = 0;
+   // console.log(runningTotal);
+   // alert("it works! you clicked on the " + operatorTextNode[2].childNodes[0].data + " operator")
+ });
+ var plusNode = document.getElementById('plus');
+ plusNode.addEventListener("click", function() {
+   displayNode.textContent = runningTotal + '+';
+   runningTotal = runningTotal + " " + operatorTextNode[3].childNodes[0].data + " ";
+   hasDecimalBeenClicked = 0;
+   // console.log(runningTotal);
+   // alert("it works! you clicked on the " + operatorTextNode[3].childNodes[0].data + " operator")
+ });
+
+ decimalNode.addEventListener("click",
+   function() {
+     // decimalNode.removeEventListener('click', removeClick);
+         if (hasDecimalBeenClicked < 1) {
+         runningTotal = runningTotal + '.';
+         displayNode.textContent = displayNode.textContent + '.';
+         hasDecimalBeenClicked++;
+       } else {
+         return;
+       }
+     });
+
+     var clearNode = document.getElementById('clear');
+     // clearNode.addEventListener('mouseover', function(){
+     //   clearNode.setAttribute('style', 'backgroundColor: #82C276;');
+     // })
+     clearNode.addEventListener("click", function() {
+       runningTotal = "";
+       sumTotal = 0;
+       hasDecimalBeenClicked = 0;
+       displayNode.textContent = "";
+       // console.log('runningTotal is now ' + runningTotal);
+       // alert("it works! You clicked the " + clearNode.attributes[1].nodeValue + " button")
+     });
+
+     var equalNode = document.getElementById('equal');
+     //add click even to equal, sum up numbers when clicked
+     equalNode.addEventListener("click", sumOfNumbers);
+
+     // get clear, viewport, equal, decimal nodes, numbers, and set up click event
 
 
  // NOTE Taken out of code, don't think it affects in any way
