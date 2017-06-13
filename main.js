1f5ead81951aacf5b5a1fef6d62504808f37874d
Christina Cornell <cdhorn515@gmail.com>
2017-06-12 23:19:17 -0400

    decimal point click once but will not reset after operaand is clicked, also calculation function not working

--- main.js
+++ main.js
@@ -6,34 +6,17 @@
 function addToDisplay(){
    var text = event.target.textContent;
    displayNode.textContent = displayNode.textContent + text;
-   runningTotal = runningTotal + " " + event.target.textContent + " ";
-   alert("it works! you clicked on the " + event.target.textContent + " button,");
+   if (text === '/' || text === '*' || text === '-' || text === '+'){
+     runningTotal = runningTotal + text + " ";
+   } else runningTotal = runningTotal + text;
+   return runningTotal;
 }
 
 function total() {
   return runningTotal = runningTotal + displayNode.textContent;
 }
 
-var runningTotal = "";
-var sumTotal = 0;
-var hasDecimalBeenClicked = 0;
-// get calculator element--outer container
-var calculatorNode = document.getElementById('calculator');
-
-// get clear, viewport, equal, decimal nodes, numbers, and set up click event
-var displayNode = document.getElementById('display');
-
-// FIXME set style for display so numbers added will show correct size and place
-if (runningTotal.length > 7) {
-  displayNode.setAttribute('style', 'fontSize: 40px;');
-} else {
-displayNode.setAttribute('style', 'fontFamily: Helvetica, sans-serif; fontWeight: lighter; fontSize: 55px; textAlign: right;');
-}
-
-
-//this works, displays all numbers to displayNode and keeps track in runningTotal
-// var displayNode = document.getElementById('display');
-var runningTotal = "";
+//gets all numbers by class, sets up click event, adds to runningTotal and to display
 var allNumbers = document.querySelectorAll('.number');
   for (var i = 0; i < allNumbers.length; i++) {
   allNumbers[i].addEventListener('click', function(){
@@ -41,55 +24,82 @@ var allNumbers = document.querySelectorAll('.number');
   });
   }
 
-  var allOperands = document.querySelectorAll('.operator');
-  for (var i = 0; i < allOperands.length; i++) {
-    allOperands[i].addEventListener('click', function(){
-      if (event.target.textContent === "C"){
-        runningTotal = "";
-        displayNode.textContent = "";
-        hasDecimalBeenClicked = 0;
-        return;
-      } if (event.target.textContent === 'X'){
-        runningTotal = runningTotal + " * ";
-        displayNode.textContent = displayNode.textContent + "*";
-        hasDecimalBeenClicked = 0;
-        return;
-     } else {
-       return addToDisplay();
+var allOperands = document.querySelectorAll('.operator');
+for (var i = 0; i < allOperands.length; i++) {
+  allOperands[i].addEventListener('click', function(){
+    if (event.target.textContent === "C"){
+      runningTotal = "";
+      displayNode.textContent = "";
+      hasDecimalBeenClicked = 0;
+      return;
+    } else if (event.target.textContent === 'X'){
+      runningTotal = runningTotal + " * ";
+      displayNode.textContent = displayNode.textContent + "*";
+      hasDecimalBeenClicked = 0;
+      return;
+    } else if (event.target.textContent === '.') {
+      if (hasDecimalBeenClicked < 1) {
+        addToDisplay();
+        hasDecimalBeenClicked++;
+      } return;
+    } else if (event.target.textContent === '=') {
+      sumOfNumbers();
+      return;
     }
+     addToDisplay();
   });
 }
-// above code for numbers and operands works!
-// when equal is clicked add all numbers together and display total
+
 function sumOfNumbers() {
   var totalArray = runningTotal.split(' ');
-  for (var i = 0; i <= totalArray.length; i++) {
+  for (var i = 0; i < totalArray.length; i++) {
     if (i === 0) {
       sumTotal = parseFloat(totalArray[i]);
-      // console.log("beginning total= " + sumTotal);
-    }
-     else if (i % 2 != 0 && totalArray[i] === '/') {
+    } else
+      if (i % 2 != 0 && totalArray[i] === '/') {
        sumTotal = sumTotal / parseFloat(totalArray[i+1]);
-      //  console.log(sumTotal);
-     }
-     else if (i % 2 != 0 && totalArray[i] === 'X') {
+    } else
+      if (i % 2 != 0 && totalArray[i] === 'X') {
        sumTotal = sumTotal * parseFloat(totalArray[i+1]);
-      //  console.log(sumTotal);
-     }
-     else if (i % 2 != 0 && totalArray[i] === '-') {
+    } else
+      if (i % 2 != 0 && totalArray[i] === '-') {
        sumTotal = sumTotal - parseFloat(totalArray[i+1]);
-      //  console.log(sumTotal);
-     }
-     else if (i % 2 != 0 && totalArray[i] === '+') {
+     } else
+       if (i % 2 != 0 && totalArray[i] === '+') {
        sumTotal = sumTotal + parseFloat(totalArray[i+1]);
-      //  console.log(sumTotal);
      }
-   } displayNode.textContent = sumTotal;
-     hasDecimalBeenClicked = 0;
-    // alert("it works! You clicked the " + equalNode.attributes[1].nodeValue + " sign");
+   } hasDecimalBeenClicked = 0;
+     displayNode.textContent = sumTotal;
   }
+
+  var totalArray;
+  var runningTotal = "";
+  var sumTotal = 0;
+  var hasDecimalBeenClicked = 0;
+  var calculatorNode = document.getElementById('calculator');
+
+
+var displayNode = document.getElementById('display');
+// FIXME set style for display so numbers added will show correct size and place
+if (runningTotal.length > 7) {
+  displayNode.setAttribute('style', 'fontSize: 40px;');
+} else {
+displayNode.setAttribute('style', 'fontFamily: Helvetica, sans-serif; fontWeight: lighter; fontSize: 55px; textAlign: right;');
+}
+
+// when equal is clicked add all numbers together and display total
+
+
+
+
+
+
+
+
+    // alert("it works! You clicked the " + equalNode.attributes[1].nodeValue + " sign");
+
 //child of calc-btn, 4 operands (+-*/)
-var operatorTextNode = document.querySelectorAll('.black-txt');
+// var operatorTextNode = document.querySelectorAll('.black-txt');
 // console.log('here '+ operatorTextNode);
 // console.log(operatorTextNode[0].childNodes[0].data);// divide (/)
 // console.log(operatorTextNode[1].childNodes[0].data);// multiply (*)
@@ -97,15 +107,15 @@ var operatorTextNode = document.querySelectorAll('.black-txt');
 // console.log(operatorTextNode[3].childNodes[0].data);// add (+)
 
 //for clear and equal FIXME for loop?
-function changeBackgroundColor() {
+// function changeBackgroundColor() {
 
   //change style.backgroundColor = the light green color
-}
-function decimalClicked(){
-  if hasDecimalBeenClicked === 0;
-  hasDecimalBeenClicked++;
- } return;
-}
+// }
+// function decimalClicked(){
+//   if hasDecimalBeenClicked === 0;
+//   hasDecimalBeenClicked++;
+//  } return;
+
 
 
 
