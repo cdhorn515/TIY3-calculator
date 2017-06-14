f462fbd6ca3078ff6b70cf3bc8c150dce7c22c8b
Christina Cornell <cdhorn515@gmail.com>
2017-06-13 23:53:37 -0400

    tried to limit decimal places to 2, changed background color to cream

--- main.js
+++ main.js
@@ -1,104 +1,111 @@
-(function(){
+(function() {
 
-"use strict";
+  "use strict";
 
-var totalArray;
-var runningTotal = "";
-var sumTotal;
-var isDecimalClicked = false;
-var calculatorNode = document.getElementById('calculator');
-var displayNode = document.getElementById('display');
+  var totalArray;
+  var runningTotal = "";
+  var sumTotal;
+  var isDecimalClicked = false;
+  var calculatorNode = document.getElementById('calculator');
+  var displayNode = document.getElementById('display');
 
 
-function addToDisplay(){
-  if (runningTotal.length >= 7) {
-    displayNode.style = 'fontSize: 40px;';
-  } else {
-  displayNode.setAttribute('style', 'fontFamily: Helvetica, sans-serif; fontWeight: lighter; fontSize: 55px; textAlign: right;');
-  }
-   var text = event.target.textContent;
-   displayNode.textContent = displayNode.textContent + text;
-   if (text === '/' || text === '*' || text === '-' || text === '+'){
-     runningTotal = runningTotal + " " + text + " ";
-   } else runningTotal = runningTotal + text;
-
-   return runningTotal;
-}
-
-function total() {
-  return runningTotal = runningTotal + displayNode.textContent;
-}
-
-//gets all numbers by class, sets up click event, adds to runningTotal and to display
-var allNumbers = document.querySelectorAll('.number');
-  for (var i = 0; i < allNumbers.length; i++) {
-  allNumbers[i].addEventListener('click', function(){
-    return addToDisplay();
-  });
-  }
-
-var allOperands = document.querySelectorAll('.operator');
-for (var i = 0; i < allOperands.length; i++) {
-  allOperands[i].addEventListener('click', function(){
-    isDecimalClicked;
-    if (event.target.textContent === "C"){
-      runningTotal = "";
-      displayNode.textContent = "";
-      return isDecimalClicked = false;
-      // return;
-    } else if (event.target.textContent === 'X'){
-      runningTotal = runningTotal + " * ";
-      // text = "*";
-      displayNode.textContent = displayNode.textContent + "*";
-       isDecimalClicked = false;
-      return;
-      // ********************************
-    } else if (event.target.textContent === "/" || event.target.textContent === "+" || event.target.textContent === "-") {
-     isDecimalClicked = false;
-    // *********************************
-    } else if (event.target.textContent === '.') {
-      if (isDecimalClicked) {
-        console.log(isDecimalClicked);
-        return;
-      } else {
-        addToDisplay();
-        return isDecimalClicked = true;
-        // return;
-      }
-    } else if (event.target.textContent === '=') {
-    return sumOfNumbers();
+  function addToDisplay() {
+    if (runningTotal.length >= 7) {
+      displayNode.style = 'fontSize: 40px;';
+    } else {
+      displayNode.setAttribute('style', 'fontFamily: Helvetica, sans-serif; fontWeight: lighter; fontSize: 55px; textAlign: right;');
     }
-     addToDisplay();
-  });
-}
+    var text = event.target.textContent;
+    displayNode.textContent = displayNode.textContent + text;
+    if (text === '/' || text === '*' || text === '-' || text === '+') {
+      runningTotal = runningTotal + " " + text + " ";
+    } else runningTotal = runningTotal + text;
 
-function sumOfNumbers() {
-  var totalArray = runningTotal.split(' ');
-  for (var i = 0; i < totalArray.length; i++) {
-    if (i === 0) {
-      sumTotal = parseFloat(totalArray[i]);
-    } else
-      if (i % 2 != 0 && totalArray[i] === '/') {
-       sumTotal = sumTotal / parseFloat(totalArray[i+1]);
-       isDecimalClicked = false;
-    } else
-      if (i % 2 != 0 && totalArray[i] === '*') {
-       sumTotal = sumTotal * parseFloat(totalArray[i+1]);
-       isDecimalClicked = false;
-    } else
-      if (i % 2 != 0 && totalArray[i] === '-') {
-       sumTotal = sumTotal - parseFloat(totalArray[i+1]);
-       isDecimalClicked = false;
-     } else
-       if (i % 2 != 0 && totalArray[i] === '+') {
-       sumTotal = sumTotal + parseFloat(totalArray[i+1]);
-       isDecimalClicked = false;
-     }
-   } isDecimalClicked = false;
-     displayNode.textContent = sumTotal;
+    return runningTotal;
   }
 
+  function total() {
+    return runningTotal = runningTotal + displayNode.textContent;
+  }
 
+  //gets all numbers by class, sets up click event, adds to runningTotal and to display
+  var allNumbers = document.querySelectorAll('.number');
+  for (var i = 0; i < allNumbers.length; i++) {
+    allNumbers[i].addEventListener('click', function() {
+      return addToDisplay();
+    });
+  }
+
+  var allOperands = document.querySelectorAll('.operator');
+  for (var i = 0; i < allOperands.length; i++) {
+    allOperands[i].addEventListener('click', function() {
+      isDecimalClicked;
+      if (event.target.textContent === "C") {
+        runningTotal = "";
+        displayNode.textContent = "";
+        return isDecimalClicked = false;
+        // return;
+      } else if (event.target.textContent === 'X') {
+        runningTotal = runningTotal + " * ";
+        // text = "*";
+        displayNode.textContent = displayNode.textContent + "*";
+        isDecimalClicked = false;
+        return;
+        // ********************************
+      } else if (event.target.textContent === "/" || event.target.textContent === "+" || event.target.textContent === "-") {
+        isDecimalClicked = false;
+        // *********************************
+      } else if (event.target.textContent === '.') {
+        if (isDecimalClicked) {
+          return;
+        } else {
+          addToDisplay();
+          return isDecimalClicked = true;
+          // return;
+        }
+      } else if (event.target.textContent === '=') {
+        return sumOfNumbers();
+      }
+      addToDisplay();
+    });
+  }
+
+  function sumOfNumbers() {
+    var totalArray = runningTotal.split(' ');
+    for (var i = 0; i < totalArray.length; i++) {
+      if (i === 0) {
+        sumTotal = parseFloat(totalArray[i]);
+      } else
+      if (i % 2 != 0 && totalArray[i] === '/') {
+        sumTotal = sumTotal / parseFloat(totalArray[i + 1]);
+        isDecimalClicked = false;
+      } else
+      if (i % 2 != 0 && totalArray[i] === '*') {
+        sumTotal = sumTotal * parseFloat(totalArray[i + 1]);
+        isDecimalClicked = false;
+      } else
+      if (i % 2 != 0 && totalArray[i] === '-') {
+        sumTotal = sumTotal - parseFloat(totalArray[i + 1]);
+        isDecimalClicked = false;
+      } else
+      if (i % 2 != 0 && totalArray[i] === '+') {
+        sumTotal = sumTotal + parseFloat(totalArray[i + 1]);
+        isDecimalClicked = false;
+      }
+    }
+
+    isDecimalClicked = false;
+    displayNode.textContent = sumTotal;
+  }
+
+  // for (var j = 0; j < sumTotal.length; j++) {
+  //    console.log(sumTotal);
+  //   if (sumTotal.charAt(j) === 46) {
+  //     console.log(sumTotal.charAt(j));
+  //     sumTotal = sumTotal.toFixed(2);
+  //   }
+  // }
 
 
 })();
